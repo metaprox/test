b83168a15bb4f49ebe8a056a566eb5c5ac204286
Sven Schley <metaprox@yahoo.de>
2023-09-28 12:23:29 +0200

    erster Button

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
